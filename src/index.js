8e7f323285b0b0638b24ea75aba5500538526dd4
Juan Carlos Díaz <169100845+Nxusbets@users.noreply.github.com>
2024-09-22 22:20:17 +0000

    hh

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 //import react into the bundle
 import React from 'react'
-import ReactDom from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'semantic-ui-css/semantic.min.css';
 
+const rootElement = document.getElementById('root'); 
 
-ReactDom.render(<App />, document.getElementById('root'));
+if (rootElement) {
+    const root = createRoot(rootElement);
+    root.render(<App />);
+} else {
+    console.error("No se encontró el elemento con id 'root'");
+}
 
 serviceWorker.unregister();
